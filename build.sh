6ec2d5a7067660465fb593bb6f774e8873e1cac6
robinshiji <robinshiji3@gmail.com>
2026-03-25 17:22:19 +0530

    build

--- build.sh
+++ build.sh
@@ -6,4 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --no-input || true
+python manage.py createsuperuser \
+  --noinput \
+  --username $DJANGO_SUPERUSER_USERNAME \
+  --email $DJANGO_SUPERUSER_EMAIL || true
