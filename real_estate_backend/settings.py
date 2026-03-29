b1085cfe12c19f1c9576d2abbabc33cb12c0b710
robinshiji <robinshiji3@gmail.com>
2026-03-29 19:29:00 +0530

    cloudinary

--- real_estate_backend/settings.py
+++ real_estate_backend/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'corsheaders',
+    'cloudinary_storage',
+    'cloudinary',
     'listings',
 ]
 
@@ -129,6 +131,14 @@ if VERCEL_URL:
     CORS_ALLOWED_ORIGINS.append(VERCEL_URL)
 
 # Media files
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
